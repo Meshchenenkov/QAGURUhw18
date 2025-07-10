352578c91907783ffd15d734d7637b9dca66485d
user <meschenenkovjob@gmail.com>
2025-07-10 23:36:09 +0700

    homework done

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+# qa_guru_python_9_jenkins
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#1. jenkins.autotests.cloud -> New Item -> вводим   НомерПотокаТрехзначный-ЛогинТелеги-НомерУрока, выбираем Freestyle project и энтер
+#2. билд
+#3. в configuration в Source Code Management выбираем Git
+#4. в Repository URL путь к корневому репозиторию в гите
+#5. в Branches to build в Branch Specifier меняем ветку с */master на /main если надо
+#6. в Build Steps -> Add build step -> Execute shell, где прописываем следующее:
+#python -m venv .venv
+#source .venv/bin/activate
+#pip install -r requirements.txt
+#pytest tests/demoqa
+#7. Restrict where this project can be run -> python
+#8. Post-build Actions -> Add post-build action -> Allure Repost
+#9. Apply
+#10. Build Now
